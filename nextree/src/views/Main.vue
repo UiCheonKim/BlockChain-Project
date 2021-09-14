7f4dc93cce2ca661ae72bcc3a17e32dc666e0d53
Welsh Corgi <86044038+UiCheonKim@users.noreply.github.com>
2021-09-14 17:56:07 +0900

    Update Main.vue

--- nextree/src/views/Main.vue
+++ nextree/src/views/Main.vue
@@ -145,59 +145,57 @@
               autoplay
               muted
               loop
+              preload="auto"
               id="myVideo"
+              style="width: 100%; object-fit: fill; position: absolute"
               class="no-top no-bottom vh-100"
             >
               <source src="@/assets/forest.mp4" type="video/mp4" />
-
-              <div class="v-center">
-                <div class="container">
-                  <div class="row align-items-center">
-                    <div class="col-md-6">
-                      <div class="spacer-single"></div>
-                      <h6 class="wow fadeInUp" data-wow-delay=".5s">
-                        <span class="text-uppercase id-color-2">Next Tree</span>
-                      </h6>
-                      <div class="spacer-10"></div>
-                      <h1 class="wow fadeInUp text-white" data-wow-delay=".75s">
-                        Create, sell or collect digital items.
-                      </h1>
-                      <p
-                        class="wow fadeInUp lead text-white"
-                        data-wow-delay="1s"
-                      >
-                        Unit of data stored on a digital ledger, called a
-                        blockchain, that certifies a digital asset to be unique
-                        and therefore not interchangeable
-                      </p>
-                      <div class="spacer-10"></div>
-                      <a
-                        href="explore.html"
-                        class="btn-main wow fadeInUp lead"
-                        data-wow-delay="1.25s"
-                        >Details</a
-                      >
-                      <a
-                        style="margin-left: 1em"
-                        href="explore.html"
-                        class="btn-main wow fadeInUp lead"
-                        data-wow-delay="1.25s"
-                        >Go to Metabus</a
-                      >
-                      <div class="mb-sm-30"></div>
-                    </div>
-                    <div class="col-md-6 xs-hide">
-                      <!-- <img
+            </video>
+            <div class="v-center">
+              <div class="container">
+                <div class="row align-items-center">
+                  <div class="col-md-6">
+                    <div class="spacer-single"></div>
+                    <h6 class="wow fadeInUp" data-wow-delay=".5s">
+                      <span class="text-uppercase id-color-2">Next Tree</span>
+                    </h6>
+                    <div class="spacer-10"></div>
+                    <h1 class="wow fadeInUp text-white" data-wow-delay=".75s">
+                      Create, sell or collect digital items.
+                    </h1>
+                    <p class="wow fadeInUp lead text-white" data-wow-delay="1s">
+                      Unit of data stored on a digital ledger, called a
+                      blockchain, that certifies a digital asset to be unique
+                      and therefore not interchangeable
+                    </p>
+                    <div class="spacer-10"></div>
+                    <a
+                      href="explore.html"
+                      class="btn-main wow fadeInUp lead"
+                      data-wow-delay="1.25s"
+                      >Details</a
+                    >
+                    <a
+                      style="margin-left: 1em"
+                      href="explore.html"
+                      class="btn-main wow fadeInUp lead"
+                      data-wow-delay="1.25s"
+                      >Go to Metabus</a
+                    >
+                    <div class="mb-sm-30"></div>
+                  </div>
+                  <div class="col-md-6 xs-hide">
+                    <!-- <img
                         src="images/misc/nft.png"
                         class="lazy img-fluid wow fadeIn"
                         data-wow-delay="1.25s"
                         alt=""
                       /> -->
-                    </div>
                   </div>
                 </div>
               </div>
-            </video>
+            </div>
           </div>
         </div>
         <button
