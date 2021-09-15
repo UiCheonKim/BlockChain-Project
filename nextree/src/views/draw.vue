f685d7b2960546453c46312a7bd396de8740be84
Welsh Corgi <86044038+UiCheonKim@users.noreply.github.com>
2021-09-15 15:50:40 +0900

    Update draw.vue

--- nextree/src/views/draw.vue
+++ nextree/src/views/draw.vue
@@ -20,81 +20,77 @@
         </div>
       </div>
     </section> -->
-  </div>
-  <br />
-  <br />
+    <section>
+      <div class="row sequence">
+        <div class="col-lg-3 col-md-6 col-sm-12 sq-item wow">
+          <div class="pricing-s1 mb30">
+            <div class="top">
+              <h2></h2>
+              <p class="plan-tagline"></p>
+            </div>
+            <div>
+              <p class="price">
+                <span class="currency"></span>
+                <span class="m opt-1"></span>
+                <span class="y opt-2"></span>
+                <span class="month"></span>
+              </p>
+            </div>
 
-  <section>
-    <div class="row sequence">
-      <div class="col-lg-3 col-md-6 col-sm-12 sq-item wow">
-        <div class="pricing-s1 mb30">
-          <div class="top">
-            <h2></h2>
-            <p class="plan-tagline"></p>
-          </div>
-          <div>
-            <p class="price">
-              <span class="currency"></span>
-              <span class="m opt-1"></span>
-              <span class="y opt-2"></span>
-              <span class="month"></span>
-            </p>
-          </div>
-
-          <div class="bottom">
-            <ul>
-              <!-- <li><i class="fa fa-check"></i>Up to 2 devices</li>
+            <div class="bottom">
+              <ul>
+                <!-- <li><i class="fa fa-check"></i>Up to 2 devices</li>
               <li><i class="fa fa-check"></i>Daily reminder</li>
               <li><i class="fa fa-check"></i>Detailed reporting</li>
               <li><i class="fa fa-check"></i>Interactive dashboard</li>
               <li><i class="fa fa-check"></i>Email and SMS notification</li>
               <li><i class="fa fa-check"></i>24/7 Customer Support</li> -->
-            </ul>
-          </div>
+              </ul>
+            </div>
 
-          <div class="action">
-            <!-- <a href="register.html" class="btn-main">Draw</a> -->
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-6 col-md-12 col-sm-12 sq-item wow">
-        <div class="pricing-s1 mb40">
-          <div class="mid text-light bg-color">
-            <span class="icon_key icon-mid"> </span>
-            <div style="margin-top: 10px"><h4>100개</h4></div>
-          </div>
-
-          <div class="top">
-            <h2>뽑기</h2>
-            <p class="plan-tagline">뭐가 나올까요?</p>
-          </div>
-
-          <div class="box-continer">
-            <div
-              class="ranbox-shake"
-              style="--bg-image: url('images/items/ranbox.png')"
-            >
-              <div
-                class="de_countdown de_countdown_center"
-                data-year="2021"
-                data-month="9"
-                data-day="16"
-                data-hour="8"
-              ></div>
+            <div class="action">
+              <!-- <a href="register.html" class="btn-main">Draw</a> -->
             </div>
           </div>
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 sq-item wow">
+          <div class="pricing-s1 mb40">
+            <div class="mid text-light bg-color">
+              <span class="icon_key icon-mid"> </span>
+              <div style="margin-top: 10px"><h4>100개</h4></div>
+            </div>
 
-          <!-- <div class="bottom"> -->
-          <!-- <ul> -->
-          <!-- <li><i class="fa fa-check"></i>Up to 2 devices</li>
+            <div class="top">
+              <h2>뽑기</h2>
+              <p class="plan-tagline">뭐가 나올까요?</p>
+            </div>
+
+            <div class="box-continer">
+              <div
+                class="ranbox-shake"
+                style="--bg-image: url('images/items/ranbox.png')"
+              >
+                <div
+                  class="de_countdown de_countdown_center"
+                  data-year="2021"
+                  data-month="9"
+                  data-day="16"
+                  data-hour="8"
+                ></div>
+              </div>
+            </div>
+
+            <!-- <div class="bottom"> -->
+            <!-- <ul> -->
+            <!-- <li><i class="fa fa-check"></i>Up to 2 devices</li>
               <li><i class="fa fa-check"></i>Daily reminder</li>
               <li><i class="fa fa-check"></i>Detailed reporting</li>
               <li><i class="fa fa-check"></i>Interactive dashboard</li>
               <li><i class="fa fa-check"></i>Email and SMS notification</li>
               <li><i class="fa fa-check"></i>24/7 Customer Support</li> -->
-          <!-- </ul> -->
-          <!-- </div> -->
-          <!-- <div
+            <!-- </ul> -->
+            <!-- </div> -->
+            <!-- <div
             class="de_countdown"
             data-year="2021"
             data-month="9"
@@ -102,53 +98,54 @@
             data-hour="8"
           ></div> -->
 
-          <div class="action" style="margin-top: 0px">
-            <a href="#top" class="btn-main large-btn font24">Draw</a>
-            <a
-              href="#top"
-              class="btn-main large-btn font24"
-              style="margin-left: 20px"
-              >Check</a
-            >
+            <div class="action" style="margin-top: 0px">
+              <a href="#top" class="btn-main large-btn font24">Draw</a>
+              <a
+                href="#top"
+                class="btn-main large-btn font24"
+                style="margin-left: 20px"
+                >Check</a
+              >
+            </div>
           </div>
         </div>
-      </div>
-      <div class="col-lg-3 col-md-6 col-sm-12 sq-item wow">
-        <div class="pricing-s1 mb30">
-          <div class="top">
-            <h2></h2>
-            <p class="plan-tagline"></p>
-          </div>
-          <div class="">
-            <p class="price">
-              <span class="currency"></span>
-              <span class="m opt-1"></span>
-              <span class="y opt-2"></span>
-              <span class="month"></span>
-            </p>
-          </div>
-          <div class="bottom">
-            <ul>
-              <!-- <li><i class="fa fa-check"></i>Up to 10 devices</li>
+        <div class="col-lg-3 col-md-6 col-sm-12 sq-item wow">
+          <div class="pricing-s1 mb30">
+            <div class="top">
+              <h2></h2>
+              <p class="plan-tagline"></p>
+            </div>
+            <div class="">
+              <p class="price">
+                <span class="currency"></span>
+                <span class="m opt-1"></span>
+                <span class="y opt-2"></span>
+                <span class="month"></span>
+              </p>
+            </div>
+            <div class="bottom">
+              <ul>
+                <!-- <li><i class="fa fa-check"></i>Up to 10 devices</li>
               <li><i class="fa fa-check"></i>Daily reminder</li>
               <li><i class="fa fa-check"></i>Detailed reporting</li>
               <li><i class="fa fa-check"></i>Interactive dashboard</li>
               <li><i class="fa fa-check"></i>Email and SMS notification</li>
               <li><i class="fa fa-check"></i>24/7 Customer Support</li> -->
-            </ul>
-          </div>
+              </ul>
+            </div>
 
-          <div class="action">
-            <a href="register.html" class=""></a>
+            <div class="action">
+              <a href="register.html" class=""></a>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="col-lg-6 offset-lg-3 text-center">
-        <small> 상자가 열리기까지 4분이 소요됩니다.</small>
+        <div class="col-lg-6 offset-lg-3 text-center">
+          <small> 상자가 열리기까지 4분이 소요됩니다.</small>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 <script>
 export default {
