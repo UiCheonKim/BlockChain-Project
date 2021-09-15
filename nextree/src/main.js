cb8ca33a23261b094c84aca424b307ea6c267381
Welsh Corgi <86044038+UiCheonKim@users.noreply.github.com>
2021-09-15 17:40:29 +0900

    style

--- nextree/src/main.js
+++ nextree/src/main.js
@@ -2,9 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store/store";
-import "../src/css/animate.css";
-import "../src/css/bootstrap.min.css";
-import "../src/css/bootstrap-grid.min.css";
-import "../src/css/bootstrap-reboot.min.css";
 
 createApp(App).use(router).use(store).use(router).mount("#app");
