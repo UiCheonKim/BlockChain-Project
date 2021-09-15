edf378d0df8d2c11d8598064d8ad22d3edeb39f9
Welsh Corgi <86044038+UiCheonKim@users.noreply.github.com>
2021-09-15 15:44:27 +0900

    about

--- nextree/src/views/about.vue
+++ nextree/src/views/about.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <div id="top"></div>
-
     <!-- section begin -->
     <section
       id="subheader"
@@ -21,79 +20,215 @@
       </div>
     </section>
     <!-- section close -->
+    <div class="main-wrapper">
+      <!-- Start About Area -->
+      <div
+        class="about-area ptb--120 ptb_md--80 ptb_sm--80 bg-white"
+        data-bg-colo="#ffffff"
+      >
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-lg-6 col-md-12 col-12">
+              <div class="about-content pr--50 pr_md--0 pr_sm--0">
+                <h2 class="mb--0">We Love Style</h2>
+                <div class="content mt--50 mt_sm--20 mt_md--20 mt_lg--20">
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book when an unknown printer took a galley of
+                    type and scrambled it to make a type specimen book.
+                  </p>
 
-    <section aria-label="section">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6 offset-md-3">
-            <form
-              name="contactForm"
-              id="contact_form"
-              class="form-border"
-              method="post"
-              action="blank.php"
-            >
-              <h3>Login to your account</h3>
-
-              <div class="field-set">
-                <label>Username</label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  class="form-control"
-                  placeholder=""
-                />
-              </div>
-
-              <div class="field-set">
-                <label>Password</label>
-                <input
-                  type="text"
-                  name="email"
-                  id="email"
-                  class="form-control"
-                  placeholder=""
-                />
-              </div>
-
-              <div id="submit">
-                <input
-                  type="submit"
-                  id="send_message"
-                  value="Login"
-                  class="btn btn-main color-2"
-                />
-
-                <div id="mail_success" class="success">
-                  Your message has been sent successfully.
+                  <p class="mt--30">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                  </p>
                 </div>
-                <div id="mail_fail" class="error">
-                  Sorry, error occured this time sending your message.
-                </div>
-
-                <div class="clearfix"></div>
-
-                <div class="spacer-single"></div>
-
-                <!-- social icons -->
-                <ul class="list s3">
-                  <li>Or login with:</li>
-                  <li><a href="#">Facebook</a></li>
-                  <li><a href="#">Google</a></li>
-                  <li><a href="#">Instagram</a></li>
-                </ul>
-                <!-- social icons close -->
               </div>
-            </form>
+            </div>
+            <div class="col-lg-6 col-md-12 col-12 mt_md--40 mt_sm--80">
+              <div class="thumbnail">
+                <iframe
+                  width="560"
+                  height="315"
+                  class="youtube-video"
+                  src="https://www.youtube.com/embed/5nI2i84hZP0"
+                  frameborder="0"
+                  allowfullscreen
+                ></iframe>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </section>
+      <!-- End About Area -->
+      <!-- Start About Area -->
+      <div
+        class="service-area pb--120 pb_md--80 pb_sm--80 bg-white"
+        data-bg-colo="#ffffff"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="section-title--3 text-center">
+                <h3 class="title">OUR SERVICES</h3>
+              </div>
+            </div>
+          </div>
+          <div class="row mt--20">
+            <!-- Start Single Service -->
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12 mt--40">
+              <div class="service">
+                <h4 class="title">Photography</h4>
+                <img src="@/assets/cogi.jpg" class="d-block w-100" alt="" />
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the.
+                </p>
+              </div>
+            </div>
+            <!-- End Single Service -->
+
+            <!-- Start Single Service -->
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12 mt--40">
+              <div class="service">
+                <h4 class="title">Digital Makerting</h4>
+                <img src="@/assets/cogi.jpg" class="d-block w-100" alt="" />
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the.
+                </p>
+              </div>
+            </div>
+            <!-- End Single Service -->
+
+            <!-- Start Single Service -->
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12 mt--40">
+              <div class="service">
+                <h4 class="title">Web Development</h4>
+                <img src="@/assets/cogi.jpg" class="d-block w-100" alt="" />
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the.
+                </p>
+              </div>
+            </div>
+            <!-- End Single Service -->
+
+            <!-- Start Single Service -->
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12 mt--40">
+              <div class="service">
+                <h4 class="title">Grapgic Design</h4>
+                <img src="@/assets/cogi.jpg" class="d-block w-100" alt="" />
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the.
+                </p>
+              </div>
+            </div>
+            <!-- End Single Service -->
+
+            <!-- Start Single Service -->
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12 mt--40">
+              <div class="service">
+                <h4 class="title">Web Development</h4>
+                <img src="@/assets/cogi.jpg" class="d-block w-100" alt="" />
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the.
+                </p>
+              </div>
+            </div>
+            <!-- End Single Service -->
+
+            <!-- Start Single Service -->
+            <div class="col-lg-4 col-md-6 col-sm-6 col-12 mt--40">
+              <div class="service">
+                <h4 class="title">Photography</h4>
+                <img src="@/assets/cogi.jpg" class="d-block w-100" alt="" />
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the.
+                </p>
+              </div>
+            </div>
+            <!-- End Single Service -->
+          </div>
+        </div>
+      </div>
+      <!-- End About Area -->
+      <div class="row">
+        <div class="col-lg-12" style="text-align: center">
+          <div></div>
+          <!-- <div class="video-with-thumb text-center">
+            <img src="@/assets/cutetest.jpg" />
+            Start Single Popup
+            <div class="video-btn position--center">
+              <a
+                class="play__btn"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+              >
+                <div class="video-icon"></div>
+              </a>
+            </div>
+            End Single Popup
+            Modal
+            <div
+              class="modal fade"
+              id="exampleModal"
+              tabindex="-1"
+              aria-labelledby="exampleModalLabel"
+              aria-hidden="true"
+            >
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button
+                      type="button"
+                      class="btn-close"
+                      data-bs-dismiss="modal"
+                      aria-label="Close"
+                      @click="pause_video()"
+                    ></button>
+                  </div>
+                  <div class="modal-body">
+                    <div class="ratio ratio-16x9">
+                      <iframe
+                        width="560"
+                        height="315"
+                        class="youtube-video"
+                        src="https://www.youtube.com/embed/5nI2i84hZP0"
+                        frameborder="0"
+                        allowfullscreen
+                      ></iframe>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> -->
+        </div>
+      </div>
+    </div>
   </div>
   <!-- content close -->
 </template>
+
 <script>
+import $ from "jquery";
+
 export default {
   name: "",
   components: {},
@@ -106,6 +241,7 @@ export default {
   created() {},
   mounted() {},
   unmounted() {},
+
   methods: {},
 };
 </script>
@@ -117,4 +253,7 @@ section {
   /* background-size: cover; */
   background-position: center;
 }
+</style>
+<style lang="css">
+@import "../assets/teststyle.css";
 </style>
