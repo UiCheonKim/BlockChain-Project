0ca94427b0cfd0331d265c532f123a0c30bd5dce
PastelWhale <43264739+HongDaeEui@users.noreply.github.com>
2021-09-23 20:39:05 +0900

    Modify: async away

--- nextree/src/views/Mypage.vue
+++ nextree/src/views/Mypage.vue
@@ -40,8 +40,9 @@
                   </div>
                   <div class="de-flex-col">
                     <a href="/rankUp" class="mybtn-main">합성</a>
-                    <a href="#" class="mybtn-main" @click="balanceOfBatch">인벤토리</a>
-
+                    <a href="#" class="mybtn-main" @click="balanceOfBatch"
+                      >인벤토리</a
+                    >
                   </div>
                 </div>
               </div>
@@ -134,8 +135,8 @@
 
                             <div class="nft__item_like">
                               <i class="fa fa-tree"></i>
-                              
-                              <span> <input v-bind:value="baby_trees"> </span>
+
+                              <span> {{ baby_trees }}</span>
                             </div>
                           </div>
                         </div>
@@ -631,14 +632,12 @@
 import Web3 from "web3";
 import dapptest from "../dapp/dapp";
 
-
-
 export default {
   name: "Mypage",
   components: {},
   data() {
     return {
-      babyTree: "",
+      babyTree: "A",
       treeInfo: "",
       user_avatar: "",
       baby_img: "/images/svg/baby.svg",
@@ -649,7 +648,7 @@ export default {
       platinum_img: "/images/svg/platinum.svg",
       diamond_img: "/images/svg/diamond.svg",
       key_img: "/images/svg/key.svg",
-      baby_trees: "1",
+      baby_trees: 1,
     };
   },
   watch: {},
@@ -657,11 +656,9 @@ export default {
   created() {
     this.userAvatar();
     this.dappstart();
-    
   },
   mounted() {
     this.balanceOfBatch();
-
   },
   unmounted() {},
   methods: {
@@ -686,8 +683,8 @@ export default {
         console.log("Injected web3 detected.");
       }
     },
-    balanceOfBatch() {
-      this.contract.methods
+    async balanceOfBatch() {
+      await this.contract.methods
         .balanceOfBatch(
           [
             this.$store.state.addr,
@@ -701,29 +698,20 @@ export default {
           [0, 1, 2, 3, 4, 5, 6]
         )
         .call()
-        .then(function (result) {
+        .then((result) => {
           console.log(result);
-         var baby_amount = result[0];
+          var baby_amount = result[0];
           console.log(baby_amount);
 
-          
-         var iron_amount = result[1];
-         var bronze_amount = result[2];
-         var silver_amount = result[3];
-         var gold_amount = result[4];
-         var platinum_amount = result[5];
-         var diamond_amount = result[6];
+          var iron_amount = result[1];
+          var bronze_amount = result[2];
+          var silver_amount = result[3];
+          var gold_amount = result[4];
+          var platinum_amount = result[5];
+          var diamond_amount = result[6];
           console.log(diamond_amount);
-
-
-         this.baby_trees = result[0];
-
+          this.baby_trees = result[0];
           console.log(this.baby_trees);
-
-
-
-
-
         });
     },
     tokenURIs() {
