595c7ab9b57c25a2d4fbdc80e70c6a5093ee234f
wofak <wodstra@protonmail.com>
2021-09-23 15:00:21 +0900

    about contents update

--- nextree/src/views/about.vue
+++ nextree/src/views/about.vue
@@ -12,7 +12,7 @@
           <div class="row">
             <div class="col-md-12 text-center">
               <h1>About</h1>
-              <p>Anim pariatur cliche reprehenderdddit</p>
+              <p>Vision, Directions, Core Values</p>
             </div>
             <div class="clearfix"></div>
           </div>
@@ -30,23 +30,19 @@
           <div class="row align-items-center">
             <div class="col-lg-6 col-md-12 col-12">
               <div class="about-content pr--50 pr_md--0 pr_sm--0">
-                <h2 class="mb--0">We Love Style</h2>
+                <h2 class="mb--0">We Act for Next Gen</h2>
                 <div class="content mt--50 mt_sm--20 mt_md--20 mt_lg--20">
                   <p>
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book when an unknown printer took a galley of
-                    type and scrambled it to make a type specimen book.
+                    우리는 항상 다음 세대의 지구를 생각합니다. <br>
+                    과거의 맑고 깨끗한 지구는 점차 그 모습을 잃어가고 있고, <br>
+                    대부분의 오염이 환경파괴와 무분별한 개발 등이 인간으로부터 비롯됩니다. <br>
+                    최근 겪어보지 못한 새로운 자연재해를 비롯하여 여러 인재들도 발생하는 가운데, <br>
+                    NexTree는 나무심기라는 지구에 생명을 불어넣는 근원적인 행동을 통해 <br>
+                    지구의 자생력을 회복시키고 다음 세대가 맑고 깨끗한 지구를 되찾도록 합니다.
                   </p>
 
                   <p class="mt--30">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book.
+                    {사업 목표, 보고서와 내용 동일하게}
                   </p>
                 </div>
               </div>
@@ -55,7 +51,7 @@
 
               <div class="thumbnail">
                 <div class="col">
-            <lottie-player src="json/earth.json"  background="transparent"  speed="1.2"  style="width: 500px; height: 500px;" autoplay loop ></lottie-player>
+            <lottie-player src="json/earth.json"  background="transparent"  speed="1.2"  style="width: 650px; height: 500px;" autoplay loop ></lottie-player>
 
 
 </div>
@@ -101,12 +97,12 @@
             <!-- Start Single Service -->
             <div class="col-lg-4 col-md-6 col-sm-6 col-12 mt--40">
               <div class="service">
-                <h4 class="title">Photography</h4>
+                <h4 class="title">Using Cryptocurrency</h4>
                 <!-- <img src="@/assets/cogi.jpg" class="d-block w-100" alt="" /> -->
                 <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the.
+                  후원에 이더리움 암호화폐를 사용합니다. 
+                  사용자는 지갑을 준비하는 것 외에는 별다른 지불 수단을 준비할 필요가 없고,
+                  개인적인 정보 또한 노출시킬 우려도 없습니다.
                 </p>
               </div>
             </div>
@@ -115,12 +111,11 @@
             <!-- Start Single Service -->
             <div class="col-lg-4 col-md-6 col-sm-6 col-12 mt--40">
               <div class="service">
-                <h4 class="title">Digital Makerting</h4>
+                <h4 class="title">NFT</h4>
                 <!-- <img src="@/assets/cogi.jpg" class="d-block w-100" alt="" /> -->
                 <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the.
+                  후원 참여자는 후원한 액수에 따라 일정한 나무 NFT를 지급 받습니다.
+                  이 NFT는 자신이 환경에 기여한 정도를 나타내며, 많이 기여 할수록 더 높은 등급의 NFT를 얻을 수 있습니다.
                 </p>
               </div>
             </div>
@@ -129,12 +124,12 @@
             <!-- Start Single Service -->
             <div class="col-lg-4 col-md-6 col-sm-6 col-12 mt--40">
               <div class="service">
-                <h4 class="title">Web Development</h4>
+                <h4 class="title">New Artist</h4>
                 <!-- <img src="@/assets/cogi.jpg" class="d-block w-100" alt="" /> -->
                 <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the.
+                  지구에 생명을 불어넣듯이, NFT라는 매개체를 사용하면서 저희는 새로운 아티스트들에게도 기회를 주고 싶었습니다.
+                  후원을 통해 얻은 뽑기권을 통해 뛰어난 아티스트들의 NFT작품을 얻을 수 있습니다.
+
                 </p>
               </div>
             </div>
@@ -143,12 +138,11 @@
             <!-- Start Single Service -->
             <div class="col-lg-4 col-md-6 col-sm-6 col-12 mt--40">
               <div class="service">
-                <h4 class="title">Grapgic Design</h4>
+                <h4 class="title">Planting Trees</h4>
                 <!-- <img src="@/assets/cogi.jpg" class="d-block w-100" alt="" /> -->
                 <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the.
+                  모든 자원이 그렇듯, 나무 또한 필요한 곳에 적절히 배분되어야만 참여자분들의 후원금이 효율적으로 지구에 도움을 줄 수 있습니다.
+                  NexTree는 정기적으로 신뢰할만한 NGO, 환경단체와의 협력을 통해 가장 필요한 곳에 나무 심기를 진행하고 있습니다.
                 </p>
               </div>
             </div>
@@ -157,12 +151,12 @@
             <!-- Start Single Service -->
             <div class="col-lg-4 col-md-6 col-sm-6 col-12 mt--40">
               <div class="service">
-                <h4 class="title">Web Development</h4>
+                <h4 class="title">Synthesis System</h4>
                 <!-- <img src="@/assets/cogi.jpg" class="d-block w-100" alt="" /> -->
                 <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the.
+                  나무 NFT는 저희 NexTree와 같이 환경사업에 참여한 지표로서 활용됩니다. 나무 NFT들은 합성이라는 과정을 통해 상위 등급으로의 전환이 가능하고
+                  상위 등급의 나무 NFT는 소유자가 환경에 기여한 정도를 한눈에 확인 할 수 있도록 합니다. 추후에 기업들이 참여할 경우 ESG트렌드에 맞춰 기업 전용
+                  서비스 또한 제공 할 예정입니다.
                 </p>
               </div>
             </div>
@@ -171,12 +165,11 @@
             <!-- Start Single Service -->
             <div class="col-lg-4 col-md-6 col-sm-6 col-12 mt--40">
               <div class="service">
-                <h4 class="title">Photography</h4>
+                <h4 class="title">Periodic report</h4>
                 <!-- <img src="@/assets/cogi.jpg" class="d-block w-100" alt="" /> -->
                 <p>
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the.
+                  NexTree는 높은 투명성을 지향합니다. 따라서 기부금이 사용되는 내역을 비롯하여 나무심기가 진행되는 과정을 최대한 생생하게 확인 해볼 수 있도록
+                  정기 보고서를 제공합니다.
                 </p>
               </div>
             </div>
